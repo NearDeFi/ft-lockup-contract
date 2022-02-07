caa5b70faa4c1619a74af91aacd26fb674d7a882
Alex Aunter <lxaunter@gmail.com>
2022-02-07 21:54:47 +0300

    test(termination): add terminate after fully vested test

--- tests/terminate.rs
+++ tests/terminate.rs
@@ -762,3 +762,81 @@ fn test_lockup_terminate_custom_vesting_during_lockup_cliff() {
     assert_eq!(lockup.claimed_balance, amount * 5 / 6);
     assert_eq!(lockup.unclaimed_balance, 0);
 }
+
+#[test]
+fn test_lockup_terminate_custom_vesting_after_vesting_finished() {
+    let e = Env::init(None);
+    let users = Users::init(&e);
+    let amount = d(60000, TOKEN_DECIMALS);
+    e.set_time_sec(GENESIS_TIMESTAMP_SEC);
+    let lockups = e.get_account_lockups(&users.alice);
+    assert!(lockups.is_empty());
+
+    let (lockup_schedule, vesting_schedule) = lockup_vesting_schedule(amount);
+    let lockup = Lockup {
+        account_id: users.alice.valid_account_id(),
+        schedule: lockup_schedule,
+        claimed_balance: 0,
+        termination_config: Some(TerminationConfig {
+            terminator_id: users.eve.valid_account_id(),
+            vesting_schedule: Some(HashOrSchedule::Schedule(vesting_schedule)),
+        }),
+    };
+
+    let balance: WrappedBalance = e.add_lockup(&e.owner, amount, &lockup).unwrap_json();
+    assert_eq!(balance.0, amount);
+    let lockups = e.get_account_lockups(&users.alice);
+    assert_eq!(lockups.len(), 1);
+    let lockup_index = lockups[0].0;
+
+    // 2Y + Y * 2 / 3, 1/8 unlocked
+    e.set_time_sec(GENESIS_TIMESTAMP_SEC + ONE_YEAR_SEC * 2 + ONE_YEAR_SEC * 2 / 3);
+    let lockups = e.get_account_lockups(&users.alice);
+    assert_eq!(lockups[0].1.total_balance, amount);
+    assert_eq!(lockups[0].1.claimed_balance, 0);
+    assert_eq!(lockups[0].1.unclaimed_balance, amount / 4);
+
+    // Trying to claim
+    ft_storage_deposit(&users.alice, TOKEN_ID, &users.alice.account_id);
+    let res: WrappedBalance = e.claim(&users.alice).unwrap_json();
+    assert_eq!(res.0, amount / 4);
+
+    // TERMINATE, 4Y, fully vested
+    e.set_time_sec(GENESIS_TIMESTAMP_SEC + ONE_YEAR_SEC * 4);
+    ft_storage_deposit(&users.eve, TOKEN_ID, &users.eve.account_id);
+    let res: WrappedBalance = e.terminate(&users.eve, lockup_index).unwrap_json();
+    assert_eq!(res.0, 0);
+    let terminator_balance = e.ft_balance_of(&users.eve);
+    assert_eq!(terminator_balance, 0);
+
+    // Checking lockup
+    let lockups = e.get_account_lockups(&users.alice);
+    assert_eq!(lockups[0].1.total_balance, amount);
+    assert_eq!(lockups[0].1.claimed_balance, amount / 4);
+    assert_eq!(lockups[0].1.unclaimed_balance, amount / 2);
+
+    // claiming
+    let res: WrappedBalance = e.claim(&users.alice).unwrap_json();
+    assert_eq!(res.0, amount / 2);
+
+    // Rewind to 4Y + 1, full unlock including part of cliff
+    e.set_time_sec(GENESIS_TIMESTAMP_SEC + ONE_YEAR_SEC * 4 + 1);
+    let lockups = e.get_account_lockups(&users.alice);
+    assert_eq!(lockups[0].1.total_balance, amount);
+    assert_eq!(lockups[0].1.claimed_balance, amount * 3 / 4);
+    assert_eq!(lockups[0].1.unclaimed_balance, amount * 1 / 4);
+
+    // Claiming
+    let res: WrappedBalance = e.claim(&users.alice).unwrap_json();
+    assert_eq!(res.0, amount * 1 / 4);
+
+    // Checking lockups
+    let lockups = e.get_account_lockups(&users.alice);
+    assert!(lockups.is_empty());
+
+    // Checking by index
+    let lockup = e.get_lockup(lockup_index);
+    assert_eq!(lockup.total_balance, amount);
+    assert_eq!(lockup.claimed_balance, amount);
+    assert_eq!(lockup.unclaimed_balance, 0);
+}
